36fa88111431b7a3a11331c19e8ef3a49196e232
Go BSInfo <go@bsinfo.eu>
2019-03-01 11:10:03 +0100

    Another change

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello again");
+		System.out.println("Upps schon geaddet...");
+		System.out.println("Noch ne Änderung.");
 	}
 }
